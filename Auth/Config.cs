7f7d0dd8cb7f757e33d98107c0974eb12285967b
Michael Marango <mike.marango@gmail.com>
2020-11-02 18:28:57 +0300

    Configure mvc client on auth server

--- Auth/Config.cs
+++ Auth/Config.cs
@@ -41,14 +41,15 @@ namespace Auth
                 // interactive client using code flow + pkce
                 new Client
                 {
-                    ClientId = "interactive",
+                    ClientId = "mvc",
+                    ClientName = "Mvc",
                     ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
                     
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:44341/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44341/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44341/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope2" }
